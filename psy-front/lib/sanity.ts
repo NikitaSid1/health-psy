1c7ce6ebb6d10b842ecd36f171768485fb7a311f
NikitaSid1 <sidmid165@gmail.com>
2026-02-18 01:34:36 +0200

    fix: restore legacy posts compatibility and fix sanity publish

--- psy-front/lib/sanity.ts
+++ psy-front/lib/sanity.ts
@@ -2,7 +2,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "dp2yjc73", // 👈 Жесткая страховка
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2024-02-16", 
   useCdn: false, // 👈 СТАВИМ FALSE! Отключаем кэш, чтобы посты появлялись мгновенно
